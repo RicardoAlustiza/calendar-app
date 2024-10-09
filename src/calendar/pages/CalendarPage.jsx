bc1716847f1adce5565bb31e0fa8adfcc1402291
Ricardo Alustiza <ricardo.alustiza@gmail.com>
2024-10-09 12:22:21 +0200

    Add react big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,46 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import {addHours, format, parse, startOfWeek, getDay} from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
+
 import { NavBar } from "../"
+
+const locales = {
+    'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+})
+
+const myEventsList = [{
+    title: 'My event',
+    notes: 'notes',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgcolor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'John Doe',
+    },
+}]
 
 export const CalendarPage = () => {
   return (
     <>
         <NavBar />
+
+        <Calendar
+            localizer={localizer}
+            events={myEventsList}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 'calc(100vh - 80px)' }}
+        />
     </>
   )
 }
