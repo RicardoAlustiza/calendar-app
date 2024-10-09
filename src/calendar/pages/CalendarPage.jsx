d3e0def983f1e7d1e6447f66fd988efd31aaa272
Ricardo Alustiza <ricardo.alustiza@gmail.com>
2024-10-09 13:17:47 +0200

    Parse lenguaje to ES & Add CalendarEvent component

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,9 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import {addHours, format, parse, startOfWeek, getDay} from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
 
-import { NavBar } from "../"
-
-const locales = {
-    'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-})
+import {addHours} from 'date-fns'
+import { CalendarEvent, NavBar } from "../"
+import { localizer, getMessagesES } from '../../helpers'
 
 const myEventsList = [{
     title: 'My event',
@@ -30,17 +18,39 @@ const myEventsList = [{
 }]
 
 export const CalendarPage = () => {
-  return (
-    <>
-        <NavBar />
 
-        <Calendar
-            localizer={localizer}
-            events={myEventsList}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 'calc(100vh - 80px)' }}
-        />
-    </>
-  )
+    const eventStyleGetter = (event, start, end, isSelected) => {
+
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white',
+        }
+
+        return {
+            style
+        }
+    }
+
+    return (
+        <>
+            <NavBar />
+
+            <Calendar
+                culture='es'
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                messages={getMessagesES()}
+                eventPropGetter={ eventStyleGetter }
+                components={{
+                    event: CalendarEvent
+                }}
+            />
+        </>
+    )
 }
