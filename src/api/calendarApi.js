a9aa9739d9fc9a36951defe7206d21c058d6f56a
Ricardo Alustiza <ricardo.alustiza@gmail.com>
2024-10-24 11:43:19 +0200

    Route to content when logIn

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -7,4 +7,21 @@ const calendarApi = axios.create({
     baseURL: VITE_API_URL
 });
 
+// Add a request interceptor
+calendarApi.interceptors.request.use(
+    (config) => {
+        // Do something before request is sent
+        config.headers = {
+            ...config.headers,
+            'x-token': localStorage.getItem('token'),
+        }
+
+        return config;
+    },
+    (error) => {
+        // Do something with request error
+        return Promise.reject(error);
+    }
+);
+
 export default calendarApi;
--- src/hooks/useAuthSotre.js
+++ src/hooks/useAuthSotre.js
@@ -49,11 +49,35 @@ export const useAuthStore = () => {
         }
     }
 
+    const checkAuthToken = async() => {
+        const token = localStorage.getItem('token');
+
+        if(!token) {
+            dispatch(onLogout());
+            return;
+        }
+
+        try {
+            const { data } = await calendarApi.get('/auth/renew');
+
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({ name: data.name, uid: data.uid }));
+        }
+        catch(error) {
+            localStorage.clear();
+            dispatch(onLogout());
+            
+            console.log(error);
+        }
+    }
+
     return {
         status,
         user,
         errorMessage,
         startLogIn,
-        startRegister
+        startRegister,
+        checkAuthToken
     }
 }
